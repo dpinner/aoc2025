779016751a2709819cebbe67f55cff770c498061
David Pinner <david@fulcrumpro.com>
2025-12-01 08:12:44 -0800

    day 1

---
+++ day1.py
@@ -0,0 +1,24 @@
+import sys
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    cur = 50
+    cnt1, cnt2 = 0, 0
+    with open(filename,'r') as f:
+        for l in f.read().splitlines():
+            d = 1 if l[0] == 'R' else -1
+            n = int(l[1:])
+            cnt2 += n // 100
+            n %= 100
+            if d == 1 and n > (100 - cur):
+                cnt2 += 1
+            elif d == -1 and n > cur > 0:
+                cnt2 += 1
+            cur += d * n
+            cur %= 100
+            if cur == 0:
+                cnt1 += 1
+            
+
+    print(cnt1)
+    print(cnt1 + cnt2)
